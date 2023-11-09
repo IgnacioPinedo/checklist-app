96ed6e92825bc686b735acd5a58b22b76a1e6730
Ignacio Pinedo <ignacio.pinedo@varsitytutors.com>
2023-11-09 15:05:39 -0800

    Initial upload

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Head from 'next/head';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+
+export default function Index() {
+  return (
+    <>
+      <Head>
+        <title>Checklist App | Home</title>
+      </Head>
+      <Container maxWidth='md' sx={{ my: { xs: 0, sm: 4 }, px: { xs: 0, sm: 4 }, py: 0 }}>
+        <Paper elevation={4} sx={{ borderRadius: { xs: 0, sm: 1 } }}>
+          <h1 style={{ margin: '0 auto', width: 'fit-content', paddingTop: '25px' }}>
+            Checklist App
+          </h1>
+          <h2 style={{ margin: '0 auto', width: 'fit-content', padding: '25px 0' }}>
+            🚧 IN CONSTRUCTION 🚧
+          </h2>
+        </Paper>
+      </Container>
+    </>
+  );
+}
