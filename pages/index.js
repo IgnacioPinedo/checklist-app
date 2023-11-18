9802545a22c679d4d4f73180453c159458e6ca40
Ignacio Pinedo <ignacio.pinedo@varsitytutors.com>
2023-11-17 17:07:29 -0800

    Remove MUI and update NextJS to 14

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import Head from 'next/head';
-import Container from '@mui/material/Container';
-import Paper from '@mui/material/Paper';
 
 export default function Index() {
   return (
@@ -9,16 +7,21 @@ export default function Index() {
       <Head>
         <title>Checklister | Home</title>
       </Head>
-      <Container maxWidth='md' sx={{ my: { xs: 0, sm: 4 }, px: { xs: 0, sm: 4 }, py: 0 }}>
-        <Paper elevation={4} sx={{ borderRadius: { xs: 0, sm: 1 } }}>
-          <h1 style={{ margin: '0 auto', width: 'fit-content', paddingTop: '25px' }}>
-            Checklister
-          </h1>
-          <h2 style={{ margin: '0 auto', width: 'fit-content', padding: '25px 0' }}>
-            🚧 UNDER CONSTRUCTION 🚧
-          </h2>
-        </Paper>
-      </Container>
+      <div
+        style={{
+          maxWidth: '900px',
+          margin: '1rem auto',
+          borderRadius: '1rem',
+          boxShadow:
+            '0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)',
+            fontFamily: 'Montserrat, sans-serif'
+        }}
+      >
+        <h1 style={{ margin: '0 auto', width: 'fit-content', paddingTop: '25px' }}>Checklister</h1>
+        <h2 style={{ margin: '0 auto', width: 'fit-content', padding: '25px 0' }}>
+          🚧 UNDER CONSTRUCTION 🚧
+        </h2>
+      </div>
     </>
   );
 }
