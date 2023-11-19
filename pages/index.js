8bd0402ef5bb154ebb9b42c9fa4c963347b83dad
Ignacio Pinedo <ignacio.pinedo@varsitytutors.com>
2023-11-19 15:06:19 -0800

    Lint

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Head from 'next/head';
 import { useState, useEffect } from 'react';
 import ChecklistList from 'src/ChecklistList';
--- src/ChecklistList.js
+++ src/ChecklistList.js
@@ -1,5 +1,3 @@
-import React, { useEffect, useState } from 'react';
-
 const ChecklistList = (props) => {
   const { checklists } = props;
 
