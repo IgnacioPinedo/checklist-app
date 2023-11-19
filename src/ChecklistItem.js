417ddce2a99ffbf710ddb709a8e49f24b7e31f01
Ignacio Pinedo <ignacio.pinedo@varsitytutors.com>
2023-11-19 15:35:53 -0800

    update styling

--- src/ChecklistItem.js
+++ src/ChecklistItem.js
@@ -2,7 +2,14 @@ const ChecklistItem = (props) => {
   const { item } = props;
 
   return (
-    <div style={{ padding: '0.5rem 3rem', borderTop: '2px solid #ddd', display: 'flex' }}>
+    <div
+      style={{
+        padding: '0.5rem 3rem',
+        borderTop: '2px solid #ddd',
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
       <input type='checkbox' style={{ marginRight: '1rem' }} />
       <h3>{item.name}</h3>
     </div>
