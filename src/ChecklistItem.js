7a4599ce33893360ddbb66a4a6fba86a87d97fb5
Ignacio Pinedo <ignacio.pinedo@varsitytutors.com>
2023-11-19 15:38:27 -0800

    Update checkbox

--- src/ChecklistItem.js
+++ src/ChecklistItem.js
@@ -1,5 +1,10 @@
+import { useState } from 'react';
+
 const ChecklistItem = (props) => {
   const { item } = props;
+
+  const [checked, setChecked] = useState(item.checked);
+
 
   return (
     <div
@@ -9,8 +14,9 @@ const ChecklistItem = (props) => {
         display: 'flex',
         alignItems: 'center',
       }}
+      onClick={(prev) => setChecked(!prev)}
     >
-      <input type='checkbox' style={{ marginRight: '1rem' }} />
+      <input type='checkbox' style={{ marginRight: '1rem' }} checked={checked} />
       <h3>{item.name}</h3>
     </div>
   );
