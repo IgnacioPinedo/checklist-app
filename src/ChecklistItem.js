c587912c6b272efb713611c44e165ffaaf4a68e5
Ignacio Pinedo <ignacio.pinedo@varsitytutors.com>
2024-02-09 17:12:57 -0800

    Add collapse and done all section

--- src/ChecklistItem.js
+++ src/ChecklistItem.js
@@ -1,14 +1,14 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from 'styles/ChecklistItem.module.css';
 
 const ChecklistItem = (props) => {
   const [checked, setChecked] = useState(false);
 
-  const { item } = props;
+  const { item, doneAll } = props;
 
   return (
     <div className={styles.item} onClick={() => setChecked((prevChecked) => !prevChecked)}>
-      <input type='checkbox' className={styles.checkbox} checked={checked} />
+      <input type='checkbox' className={styles.checkbox} checked={checked || doneAll} />
       <h3>{item.name}</h3>
     </div>
   );
--- src/ChecklistSection.js
+++ src/ChecklistSection.js
@@ -1,17 +1,42 @@
+import { useState } from 'react';
 import ChecklistItem from 'src/ChecklistItem';
 import styles from 'styles/ChecklistSection.module.css';
 
 const ChecklistSection = (props) => {
+  const [collapsed, setCollapsed] = useState(false);
+  const [doneAll, setDoneAll] = useState(false);
   const { section } = props;
+
+  const toggleCollapsed = () => {
+    setCollapsed((prevState) => !prevState);
+  };
+
+  const toggleDoneAll = () => {
+    setDoneAll((prevDoneAll) => !prevDoneAll);
+  };
 
   return (
     <>
       <div className={styles.header}>
         <h2>{section.name}</h2>
+        <div className={styles['form-icons']}>
+          <span class='material-symbols-outlined' onClick={toggleDoneAll}>done_all</span>
+          {collapsed ? (
+            <span className='material-symbols-outlined' onClick={toggleCollapsed}>
+              expand_less
+            </span>
+          ) : (
+            <span class='material-symbols-outlined' onClick={toggleCollapsed}>
+              expand_more
+            </span>
+          )}
+        </div>
       </div>
-      {section.items.map((item) => (
-        <ChecklistItem key={item.id} item={item} />
-      ))}
+      <div className={collapsed ? styles.collapsed : styles.expanded}>
+        {section.items.map((item) => (
+          <ChecklistItem key={item.id} item={item} doneAll={doneAll} />
+        ))}
+      </div>
     </>
   );
 };
